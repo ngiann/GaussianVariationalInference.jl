e4170a89e72785390bcd4c504adb576f1e803451
ngiann <ngiann@gmail.com>
2020-10-21 10:00:20 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/ApproximateVI.jl
@@ -0,0 +1,5 @@
+module ApproximateVI
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ApproximateVI
+using Test
+
+@testset "ApproximateVI.jl" begin
+    # Write your tests here.
+end
