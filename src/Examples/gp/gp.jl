3ec05aa962e13f09bedc027412bff848076a37e9
ngiann <ngiann@gmail.com>
2020-10-21 21:23:17 +0200

    new gp example

---
+++ src/Examples/gp/gp.jl
@@ -0,0 +1,54 @@
+using AbstractGPs, PyPlot, Printf, LinearAlgebra
+
+# Define log-likelihood
+
+function logp(θ; x=x, y=y)
+
+    local kernel = ScaledKernel(transform(Matern52Kernel(), ScaleTransform(exp(θ[1]))),exp(θ[2]))
+
+    local f = GP(kernel)
+
+    local fx = f(x, exp(θ[3]))
+
+    logpdf(fx, y)
+
+end
+
+# Generate some synthetic data
+
+N = 25
+
+σ = 0.1
+
+x = rand(N)*10
+
+y = sin.(x) .+ randn(N)*σ
+
+xtest = collect(-1.0:0.1:11.0)
+ytest = sin.(xtest)
+
+
+# Approximate posterior with Gaussian
+
+postθ, = VI( θ ->  logp(θ; x=x, y=y), [randn(3)*2 for i=1:10], S = 200, iterations = 100, show_every=10)
+
+
+# Draw samples from posterior and plot
+
+for i in 1:3
+    θ = rand(postθ)
+    sample_kernel = ScaledKernel(transform(Matern52Kernel(), ScaleTransform(exp(θ[1]))),exp(θ[2]))
+    f = GP(sample_kernel)
+    p_fx = posterior(f(x, exp(θ[3])), y)
+    μ, Σ = mean_and_cov(p_fx, xtest)
+
+    figure()
+    plot(x, y, "ko",label="Train Data")
+    plot(xtest, ytest, "b-", label="Test Data")
+
+    plot(xtest, μ, "r-")
+    fill_between(xtest, μ.-sqrt.(diag(Σ)),μ.+sqrt.(diag(Σ)), color="r", alpha=0.3)
+
+    title(@sprintf("GP posterior, sampled hyperparameters %.2f, %.2f, %.2f", exp(θ[1]),exp(θ[2]),exp(θ[3])))
+    legend()
+end
