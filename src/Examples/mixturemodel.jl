778e40147407613dc60fbf1c17830e772b3744e0
ngiann <ngiann@gmail.com>
2021-06-05 14:32:31 +0200

    updated example - works as expected

--- src/Examples/mixturemodel.jl
+++ src/Examples/mixturemodel.jl
@@ -6,10 +6,10 @@ using PyPlot
 
 # Define means for three-component Gaussian mixture model
 # All components are implicitly equally weighted and have unit covariance
-μ = [zeros(2), [2.5; 0.0], [-2.5; 1.2]]
+μ = [zeros(2), [2.9; 0.0], [2.5; 1.2]]
 
 # Define log-likelihood
-logp(θ) = log(exp(-0.5*sum((μ[1].-θ).^2)) + exp(-0.5*sum((μ[1].-θ).^2)) + exp(-0.5*sum((μ[3].-θ).^2)))
+logp(θ) = log(exp(-0.5*sum((μ[1].-θ).^2)) + exp(-0.5*sum((μ[2].-θ).^2)) + exp(-0.5*sum((μ[3].-θ).^2)))
 
 # Plot target density
 figure()
@@ -19,17 +19,19 @@ contourf( repeat(θval,1,length(θval)),  repeat(θval',length(θval),1), map(θ
 axis("equal")
 
 # Approximate mixture model with Gaussian density
-posteriorfull, =      VI(logp, randn(2); S = 150, iterations = 100)
-posteriordiag, =  VIdiag(logp, randn(2); S = 150, iterations = 100)
-posteriorlap,  = laplace(logp, randn(2); iterations = 100)
-posteriormvi,  =     MVI(logp, posteriorlap; S = 150, iterations = 100)
+posteriorfull,   =         VI(logp, randn(2); S = 150, iterations = 100)
+posteriordiag,   =     VIdiag(logp, randn(2); S = 150, iterations = 100)
+posteriorfixC,   = VIfixedcov(logp, randn(2), 1.0*Matrix(I, 2, 2); S = 150, iterations = 100)
+posteriorlap,    =    laplace(logp, randn(2); iterations = 100)
+posteriormvi,    =        MVI(logp, posteriorlap; S = 150, iterations = 100)
 # posteriormvidiag,  = MVI(logp, MvNormal(zeros(2),Matrix(I,2,2)); S = 150, iterations = 100)
 
 # Plot our approximations
-ApproximateVI.plot_ellipse(posteriorfull, "b", "full")
-ApproximateVI.plot_ellipse(posteriordiag, "r", "diag")
-ApproximateVI.plot_ellipse(posteriormvi,  "g", "mvi")
-ApproximateVI.plot_ellipse(posteriorlap,  "m", "laplace")
+ApproximateVI.plot_ellipse(posteriorfull,   "b", "full")
+ApproximateVI.plot_ellipse(posteriordiag,   "r", "diag")
+ApproximateVI.plot_ellipse(posteriorfixC,   "c", "sphere-fix")
+ApproximateVI.plot_ellipse(posteriormvi,    "g", "mvi")
+ApproximateVI.plot_ellipse(posteriorlap,    "m", "laplace")
 # ApproximateVI.plot_ellipse(posteriormvidiag,  "k", "diag by mvi")
 
 legend();
