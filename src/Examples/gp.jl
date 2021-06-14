4e44a149aad10d91dc70b405b6a070e52360a0f9
ngiann <ngiann@gmail.com>
2021-06-14 21:50:24 +0200

    variables in for loop are now explicitly declared as local ones

--- src/Examples/gp.jl
+++ src/Examples/gp.jl
@@ -48,15 +48,15 @@ postθ, = VI( θ ->  logp(θ; x=x, y=y), [randn(3)*2 for i=1:10], S = 200, itera
 for i in 1:3
 
     # draw hyperparameter sample from approximating Gaussian distribution
-    θ = rand(postθ)
+    local θ = rand(postθ)
 
     # instantiate kernel
-    sample_kernel = exp(θ[2]) * (Matern52Kernel() ∘ ScaleTransform(exp(θ[1])))
+    local sample_kernel = exp(θ[2]) * (Matern52Kernel() ∘ ScaleTransform(exp(θ[1])))
 
     # intantiate kernel, GP object and calculate posterior mean and covariance for the training data x, y generated above
-    f = GP(sample_kernel)
-    p_fx = AbstractGPs.posterior(f(x, exp(θ[3])), y)
-    μ, Σ = AbstractGPs.mean_and_cov(p_fx, xtest)
+    local f = GP(sample_kernel)
+    local p_fx = AbstractGPs.posterior(f(x, exp(θ[3])), y)
+    local μ, Σ = AbstractGPs.mean_and_cov(p_fx, xtest)
 
     figure()
     plot(x, y, "ko",label="Training data")
