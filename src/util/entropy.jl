220d91fbfa406ae23d8600bc8ec65d45da0f6e85
ngiann <ngiann@gmail.com>
2022-11-17 10:55:19 +0100

    moved file

--- src/util/entropy.jl
+++
@@ -1,5 +0,0 @@
-entropy(C::Array{T, 2} where T<:Real) = 0.5*logdet(2*π*ℯ*C*C')
-
-entropy(C::AbstractArray{T, 1} where T<:Real) = 0.5*sum(log.(C.^2)) + 0.5*log(2*π*ℯ) * length(C)
-
-entropy(σ::Real) = 0.5*log(2*π*ℯ*σ^2)
