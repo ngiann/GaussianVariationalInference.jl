376b6414d064b8eeae1567c7f35195e96af9d6d1
ngiann <ngiann@gmail.com>
2023-05-09 10:09:16 +0200

    minor in docstring

--- src/Examples/verificationexample1.jl
+++ src/Examples/verificationexample1.jl
@@ -3,12 +3,12 @@
     
 # Example
 ```julia-repl
-julia> logp, f, g = verificationexample1() # target distribution to approximate
-julia> q, logev = VI(logp, randn(2), S = 100, iterations = 10_000, show_every = 50, transform = f)
-julia> using Plots # must be indepedently installed.
-julia> x = -3:0.02:3
-julia> contour(x, x, map(x -> exp(logp(collect(x))), Iterators.product(x, x))', fill=true, c=:blues, colorbar = false) # plot target
-julia> contour!(x, x, map(x -> pdf(q,(collect(x))), Iterators.product(x, x))', color="red", alpha=0.3) # plot approximation q
+    julia> logp, f, g = verificationexample1() # target distribution to approximate
+    julia> q, logev = VI(logp, randn(2), S = 100, iterations = 10_000, show_every = 50, transform = f)
+    julia> using Plots # must be indepedently installed.
+    julia> x = -3:0.02:3
+    julia> contour(x, x, map(x -> exp(logp(collect(x))), Iterators.product(x, x))', fill=true, c=:blues, colorbar = false) # plot target
+    julia> contour!(x, x, map(x -> pdf(q,(collect(x))), Iterators.product(x, x))', color="red", alpha=0.3) # plot approximation q
 ```
 """
 function verificationexample1()
