f9c3d49790808043d4f366f9a10cb4a74fd15ab2
ngiann <ngiann@gmail.com>
2023-05-09 09:31:20 +0200

    avoid testing elbo on first iteration

--- src/util/RecordELBOProgress.jl
+++ src/util/RecordELBOProgress.jl
@@ -66,7 +66,7 @@ function (p::RecordELBOProgress)(os) # used as callback
     # end
 
 
-    if p.test_every > 0 && mod(iteration, p.test_every) == 0
+    if p.test_every > 0 && mod(iteration+1, p.test_every) == 0
        
         currelbotest, currelbotest_std, numsamples = p.testelbofunction(currentminimizer)
 
