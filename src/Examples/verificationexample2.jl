93a0c006389d9bc5eefc975d0f5dce5263d7e511
ngiann <ngiann@gmail.com>
2023-06-12 13:43:43 +0200

    example for verifying VI under change of variables

---
+++ src/Examples/verificationexample2.jl
@@ -0,0 +1,34 @@
+"""
+    Synthetic two-dimensional problem for verifying transform
+    
+# Example
+```julia-repl
+julia> logp, sample, f, g = verificationexample2() # target distribution to approximate
+julia> q, logev = VI(logp, randn(2), S = 100, iterations = 10_000, show_every = 50, transform = f)
+julia> using Plots # must be indepedently installed.
+julia> x = 0.001:0.02:5
+julia> contour(x, x, map(x -> exp(logp(collect(x))), Iterators.product(x, x))', fill=true, c=:blues, colorbar = false) # plot target
+julia> contour!(x, x, map(x -> pdf(q,(collect(x))), Iterators.product(x, x))', color="red", alpha=0.3) # plot approximation q
+```
+"""
+function verificationexample2()
+
+    Σdiag = [2.0 0.0; 0.0 1] # diagonal matrix
+    
+    μ = [3.1; 0.2]
+
+    g(y) = log.(y)
+
+    f(x) = exp.(x)
+
+    basedensity = MvNormal(μ, Σdiag)
+
+    jac(x) = ForwardDiff.jacobian(g, x)
+
+    log_transformeddensity =  x-> logpdf(basedensity, g(x)) + logabsdet(jac(x))[1]
+
+    sample() = f(rand(basedensity))
+    
+    return log_transformeddensity, sample, f, g
+    
+end
