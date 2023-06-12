b0903d5daab02ba795c727fc44ef46b3e022714a
ngiann <ngiann@gmail.com>
2023-06-12 13:43:13 +0200

    auxiliaries for calculating derivatives

---
+++ src/util/first_second_derivative.jl
@@ -0,0 +1,14 @@
+firstderivative(f, x)  = ForwardDiff.derivative(f,x)
+
+secondderivative(f, x) = ForwardDiff.derivative(firstderivativefunction(f), x)
+
+
+firstderivativefunction(f)  = x -> ForwardDiff.derivative(f, x)
+
+function secondderivativefunction(f) 
+    
+    aux = firstderivativefunction(f)
+    
+    firstderivativefunction(aux)
+
+end
