897d0b4088d96e2dcb7b78134e24a4e227e73dfa
ngiann <ngiann@gmail.com>
2023-07-20 08:47:46 +0200

    eigendecomposition

--- src/util/laplace.jl
+++ src/util/laplace.jl
@@ -68,3 +68,20 @@ function get_covariance_at_mode(mode, negative_logposterior)
    return (Σ + Σ') / 2
 
 end
+
+#----------------------------------------------------
+function eigendecomposition(Σ)
+#----------------------------------------------------
+   
+       # decompose covariance
+       EigenDecomposition = eigen(Σ)
+   
+       # get root of eigenvalues
+       Esqrt = sqrt.(max.(EigenDecomposition.values, 1e-8))
+   
+       # get eigenvectors that stay fixed throughout
+       V     = EigenDecomposition.vectors
+   
+       return V, Esqrt
+   
+   end
